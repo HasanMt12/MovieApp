7f3a202ec7cdf703e748f10c77d3bf0ab41b8da8
iamtonmoy0 <tonmoy2code@gmail.com>
2023-06-03 04:38:27 -0400

    import file added

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import './index.css'
 import {  RouterProvider, createBrowserRouter } from 'react-router-dom'
 import * as RoutePath from './routes/route'
 import App from './App'
-import { Home } from './pages/Home'
-
-
+import { Home } from './pages/Home-pages/Home'
+import New from './pages/New'
+import Upcoming from './pages/Upcoming'
+import Recommended from './pages/Recommended'
+import Login from '../src/components/Login/Login'
 
 
 // Routes 
