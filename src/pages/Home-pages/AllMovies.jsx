aa1a9b4d6cd2ab2998302fa9e7e9c903e9e4f5cd
Hasan Mt <hasan.2mahmud22@gmail.com>
2023-06-03 16:48:09 +0600

    refector

--- src/pages/Home-pages/AllMovies.jsx
+++ src/pages/Home-pages/AllMovies.jsx
@@ -35,10 +35,12 @@ const AllMovies = () => {
                     }
                 </MovieSection>
                 <FlexCenter className="mt-4">
-                     <PrimaryButton onClick={() => setLimit(limit + 2)}>show more
-                    </PrimaryButton>
+                     <button onClick={() => setLimit(limit + 2)} className="rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-red-600/80 text-red-600/80 text-white">
+                        <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-red-600/80 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
+                        <span className="relative text-red-600/80 transition duration-300 group-hover:text-white ease">Show More</span>
+                    </button>
                 </FlexCenter>
-               
+
             </Container>
             
         </>
